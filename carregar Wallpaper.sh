4b75b9f24a43cb34ca97ebb3057444d6dd0f266f
Maykon Deykon <maykon@SSPPSSTISDS05.SSP>
2014-05-20 10:57:41 -0300

    atualização do código

--- carregar Wallpaper.sh
+++ carregar Wallpaper.sh
@@ -16,6 +16,7 @@ wallpaperName="Wallpaper.jpg"
 mkdir -p ${temp}
 mkdir -p ${dirLocal}
 sudo mount ${pathWallPaper} ${temp} -o credentials=${credential}/${ssp},file_mode=0777,dir_mode=0777
+# sudo mount ${pathWallPaper} ${temp} -o user=${user},password=${pass},file_mode=0777,dir_mode=0777
 cp ${temp}/Wallpaper.jpg ${dirLocal}
 gsettings set org.gnome.desktop.background picture-uri file://${dirLocal}/${wallpaperName}
 
