c29ca3210973e6ff144a0dc1834fa38f45a8f138
Maykon Deykon <maykon@SSPPSSTISDS05.SSP>
2014-05-20 10:54:32 -0300

    atualização das credenciais

--- carregar Wallpaper.sh
+++ carregar Wallpaper.sh
@@ -4,6 +4,9 @@
 user="SSP/user"
 # Informe aqui sua senha do domínio
 pass="pass"
+# Ou informe o arquivo de credenciais
+credential=$HOME/.credentials
+ssp=sspCredential
 
 pathWallPaper="//172.20.1.111/wallpaper/"
 temp=$HOME/"wallpaperTemp"
@@ -12,7 +15,7 @@ wallpaperName="Wallpaper.jpg"
 
 mkdir -p ${temp}
 mkdir -p ${dirLocal}
-sudo mount ${pathWallPaper} ${temp} -o credentials=/home/maykon/.credentials/sspCredential,file_mode=0777,dir_mode=0777
+sudo mount ${pathWallPaper} ${temp} -o credentials=${credential}/${ssp},file_mode=0777,dir_mode=0777
 cp ${temp}/Wallpaper.jpg ${dirLocal}
 gsettings set org.gnome.desktop.background picture-uri file://${dirLocal}/${wallpaperName}
 
