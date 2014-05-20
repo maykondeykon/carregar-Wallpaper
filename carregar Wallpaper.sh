be02cb6d8dac9881916c293e724c8cf5fd32363c
Maykon Deykon <maykon@SSPPSSTISDS05.SSP>
2014-05-20 10:44:57 -0300

    versão com credenciais

--- carregar Wallpaper.sh
+++ carregar Wallpaper.sh
@@ -9,11 +9,14 @@ pathWallPaper="//172.20.1.111/wallpaper/"
 temp=$HOME/"wallpaperTemp"
 dirLocal=$HOME/".wallpaperSuinf"
 wallpaperName="Wallpaper.jpg"
+
 mkdir -p ${temp}
 mkdir -p ${dirLocal}
-sudo mount.cifs ${pathWallPaper} ${temp} -o user=${user},password=${pass},file_mode=0777,dir_mode=0777
+sudo mount ${pathWallPaper} ${temp} -o credentials=/home/maykon/.credentials/sspCredential,file_mode=0777,dir_mode=0777
 cp ${temp}/Wallpaper.jpg ${dirLocal}
 gsettings set org.gnome.desktop.background picture-uri file://${dirLocal}/${wallpaperName}
+
 sleep 1
 sudo umount ${temp}/
 rm -Rf ${temp}
+
